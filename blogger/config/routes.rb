ea35ed9105190624ba69d9885c3676b8f33f64da
PaulOkoh <pauldokoh@gmail.com>
2016-12-25 07:26:31 -0600

    Tagging feature completed

--- blogger/config/routes.rb
+++ blogger/config/routes.rb
@@ -1,7 +1,8 @@
 Blogger::Application.routes.draw do
 	resources :articles  do
 		resources :comments
-		
 	end
+	resources :tags
 end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	
